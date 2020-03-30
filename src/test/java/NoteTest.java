a65c8583c588f9fc9ca7a16fba34486cb04bbcb9
crazyiwan <chongshenn@gmail.com>
2020-03-30 14:44:19 +0200

    First exercise

---
+++ src/test/java/NoteTest.java
@@ -0,0 +1,67 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class NoteTest {
+
+    @Test
+    void testGetName(){
+        assertThat(Note.of("Jarek", 4).getName()).isEqualTo("Jarek");
+    }
+
+    @Test
+    void testGetNote(){
+        assertThat(Note.of("Jarek", 4).getNote()).isEqualTo(4);
+    }
+
+    @Test
+    void testEmptyName(){
+        assertThatThrownBy(() -> {
+            Note.of("", 4);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Imię ucznia nie może być puste");
+    }
+
+    @Test
+    void testNullName(){
+        assertThatThrownBy(() -> {
+            Note.of(null, 4);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Imię ucznia nie może być null");
+    }
+
+    @Test
+    void testTrimWhitespaces(){
+        assertThatThrownBy(() -> {
+            Note.of(" \n \t \n    ", 4);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Imię ucznia nie może być puste");
+    }
+
+    @Test
+    void testGradeOverRange(){
+        assertThatThrownBy(() -> {
+            Note.of("Marek", 8);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Niewłaściwa ocena");
+    }
+
+    @Test
+    void testGradeBelowRange(){
+        assertThatThrownBy(() -> {
+            Note.of("Czarek", 1);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Niewłaściwa ocena");
+    }
+
+    @Test
+    void testNegativeGrade(){
+        assertThatThrownBy(() -> {
+            Note.of("Kanarek", -3);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Niewłaściwa ocena");
+    }
+    
+}
